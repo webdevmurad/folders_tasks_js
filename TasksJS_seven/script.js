61def79931d11f9407ade9e86eb2a17bfd8fc1c8
Murad <murad-arslangiraev-1994@mail.ru>
2019-10-09 16:02:38 +0300

    сделал еще задач к unit 7

--- TasksJS_seven/script.js
+++ TasksJS_seven/script.js
@@ -46,3 +46,79 @@ let btn5 = document.querySelector('.btn5'),
 btn5.onclick = () => {
     alert(inp5.value);
 }
+
+// Task 6
+// Вывод на экран
+
+let btn6 = document.querySelector('.btn6'),
+    inp6_1 = document.querySelector('.inp6_1'),
+    inp6_2 = document.querySelector('.inp6_2');
+
+btn6.onclick = () => {
+    inp61 = +inp6_1.value;
+    inp62 = +inp6_2.value;
+
+    if (inp61 > inp62) {
+        alert(inp61);
+    } 
+    else if (inp61 < inp62) {
+        alert(inp62);
+    }
+    else if (inp61 == inp62) {
+        alert(inp61 || inp62);
+    }
+}
+
+// Task 7
+// Вывод на экран
+
+let btn7 = document.querySelector('.btn7'),
+    inp7 = document.querySelector('.inp7');
+
+btn7.onclick = () => {
+    inpVal = +inp7.value;
+    alert(2019 - inpVal);
+}
+
+// Task 8
+// Вывод на экран
+
+let btn8 = document.querySelector('.btn8');
+
+btn8.onclick = () => {
+    function randomInteger(min, max) {
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+    }
+    alert(randomInteger(1, 10));
+}
+
+// Task 9
+// Вывод на экран
+
+let btn9 = document.querySelector('.btn9'),
+    inp9_1 = document.querySelector('.inp9_1'),
+    inp9_2 = document.querySelector('.inp9_2');
+
+btn9.onclick = () => {
+    inp91 = +inp9_1.value;
+    inp92 = +inp9_2.value;
+    function randomInteger(min, max) {
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+    }
+    alert(randomInteger(inp91, inp92));
+}
+
+// Task 10
+// Вывод в консоль 
+
+let btn10 = document.querySelector('.btn10');
+
+function color(){
+    return Math.floor(Math.random() * (255 - 0)) + 0;
+}
+
+btn10.onclick = () => {
+    console.log( 'rgba' + '(' + color() + ',' + color() + ',' + color() + ');');
+}
