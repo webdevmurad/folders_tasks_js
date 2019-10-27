7d75579b7ee79bec3a68d3d757a615e4acfd2850
Murad <murad-arslangiraev-1994@mail.ru>
2019-10-27 16:35:06 +0300

    сделал несколько тасков

---
+++ TasksJS_thirteen/script.js
@@ -0,0 +1,127 @@
+// Task 1.
+// Выведите массив a1 на страницу.
+
+// a1 = {
+//  3 : 'hello',
+//  'one' : 'hi'
+// };
+
+let out1 = document.querySelector('.out-1');
+let out11 = '';
+let a1 = {
+    3 : 'hello',
+    'one' : 'hi'
+};
+for (let key in a1) {
+    out11 += key + ' : ' + a1[key] + '<br>';
+}
+out1.innerHTML = out11;
+
+// Task 2.
+// Выведите на страницу элементы из масиива a2 у которых символов больше 4.
+
+// a2 = {
+//  3 : 'hello',
+//  'one' : 'hi',
+//  'testt' : 'vodoley',
+//  'ivan' : 'ivanov'
+// };
+
+let out2 = document.querySelector('.out-2');
+let a2 = {
+    3 : 'hello',
+    'one' : 'hi',
+    'testt' : 'vodoley',
+    'ivan' : 'ivanov'
+};
+
+
+for (let key in a2) {
+    if(a2[key].length > 4) {
+        out2.innerHTML += a2[key] + '<br>';
+    }
+}
+
+// Task 3.
+// Выведите на страницу элементы из масиива a3 у которых ключ содержит больше 4 символов.
+
+// a3 = {
+//  3 : 'hello',
+//  'one' : 'hi',
+//  'testt' : 'vodoley',
+//  'ivan' : 'ivanov'
+// };
+
+let out3 = document.querySelector('.out-3');
+a3 = {
+    3 : 'hello',
+    'one' : 'hi',
+    'testt' : 'vodoley',
+    'ivan' : 'ivanov'
+};
+
+for (let key in a3) {
+    if(a3[key].length < 4) {
+        out3.innerHTML += a3[key] + '<br>';
+    }
+}
+
+// Task 4.
+// Выведите на страницу элементы из масиива a4 у которых значение - число.
+
+// a4 = {
+//     3 : 'hello',
+//     'one' : 4,
+//     'testt' : 'vodoley',
+//     'ivan' : 6
+// };
+
+let out4 = document.querySelector('.out-4');
+a4 = {
+    3 : 'hello',
+    'one' : 4,
+    'testt' : 'vodoley',
+    'ivan' : 6
+};
+
+for (let key in a4) {
+    if(typeof(a4[key]) == "number") {
+        out4.innerHTML += a4[key] + '<br>';
+    }
+}
+
+// Task 5.
+// Дан ассоциативный массив a5. Найдите сумму элементов находящихся в нем.
+
+// a5 = {
+//  a : 7,
+//  z : 4,
+//  45 : 12,
+//  f : 6
+// };
+
+let out5 = document.querySelector('.out-5');
+let sum5 = 0;
+a5 = {
+    a : 7,
+    z : 4,
+    45 : 12,
+    f : 6
+};
+
+for (let key in a5) {
+    out5.innerHTML = sum5 += a5[key];
+}
+
+// Task 6.
+// Создайте ассоциативный массив a6, который содержит 
+// ключи name, age, sex, height и значения любого персонажа. 
+// Выведите массив на страницу.
+
+let out6 = document.querySelector('.out-6');
+let a6 = {
+    "name": "Murad",
+    "age": 25,
+    "sex": "male",
+    "height": 178
+}
