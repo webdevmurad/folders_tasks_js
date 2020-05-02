4cca916c6ca528d8c9ec113f44ae78c03e6df5e4
Murad <murad-arslangiraev-1994@mail.ru>
2020-05-02 17:22:29 +0300

    задачи по теме функции

--- eloquentJS/Chapter 3. Function/script.js
+++ eloquentJS/Chapter 3. Function/script.js
@@ -5,7 +5,35 @@ function min(a, b) {
     else return b;
 }
 
-console.log(min(4, 5))
+// console.log(min(4, 5))
 
 //  Рекурсия 
 
+function isEven(n) {
+    if (n == 0) {
+        return true;
+    } else if (n == 1) {
+        return false;
+    } else if (n < 0) {
+        return isEven(-n);
+    } else return isEven(n - 2);
+}
+
+// console.log(isEven(50))
+// console.log(isEven(75))
+// console.log(isEven(-1))
+
+// Подсчет букв 
+
+function countChar(string, ch) {
+    let count = 0;
+    for(let i = 0; i < string.length; i++) {
+        if (string[i] == ch) {
+            count++;
+        }
+    }
+
+    return count
+}
+
+// console.log(countChar('murad', 'a'))
